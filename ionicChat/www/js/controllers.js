af25bd81d07fddebb60548a26ce43521eb565a37
Austin Riba <root@austinriba.com>
2015-01-20 21:45:18 +0200

    initial commit

---
+++ ionicChat/www/js/controllers.js
@@ -0,0 +1,26 @@
+angular.module('starter.controllers', [])
+
+
+.controller('ChatsCtrl', function($rootScope, $scope, Chats) {
+    $scope.chats = []
+
+    var exampleSocket = new WebSocket("ws://localhost:8765/");
+    exampleSocket.onopen = function (event) {
+
+        console.log("socket open")
+    };
+    exampleSocket.onmessage = function (event) {
+        console.log("recieved")
+        $scope.chats.push(JSON.parse(event.data));
+        $scope.$apply();
+    }
+
+    $scope.sendMessage = function(message){
+        console.log("sent message" + message)
+        exampleSocket.send(JSON.stringify({"name": $rootScope.settings.username, "msg": message}));
+    }
+})
+
+
+.controller('AccountCtrl', function($rootScope) {
+});
---
+++ server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import asyncio
+import websockets
+
+
+class BroadcastServer:
+
+    def __init__(self):
+        self.clients = []
+
+    def run(self):
+        start_server = websockets.serve(self.handler, 'localhost', 8765)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
+
+    @asyncio.coroutine
+    def handler(self, websocket, path):
+        self.clients.append(websocket)
+        while True:
+            message = yield from websocket.recv()
+            if message is None:
+                self.clients.remove(websocket)
+                break
+            yield from self.broadcast(message)
+
+
+    @asyncio.coroutine
+    def broadcast(self, text):
+        print("broadcast")
+        for client in self.clients:
+            print("|| {}".format(text))
+            yield from client.send(text)
+
+bs = BroadcastServer()
+bs.run()
