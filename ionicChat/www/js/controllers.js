414c96185b501ecafb30576a45e9f65be8d6d768
Austin Riba <root@austinriba.com>
2015-01-21 20:50:34 +0200

    chat log

--- ionicChat/www/js/controllers.js
+++ ionicChat/www/js/controllers.js
@@ -3,6 +3,10 @@ angular.module('starter.controllers', [])
 
 .controller('ChatsCtrl', function($scope, $ionicScrollDelegate, $window) {
     $scope.chats = [];
+    if($window.localStorage.chats){
+        lchats = JSON.parse($window.localStorage.chats);
+        $scope.chats = lchats;
+    }
 
     var exampleSocket = new WebSocket("ws://getinstabot.com:8765/");
     exampleSocket.onopen = function (event) {
@@ -14,14 +18,18 @@ angular.module('starter.controllers', [])
     exampleSocket.onmessage = function (event) {
         console.log("recieved");
         $scope.chats.push(JSON.parse(event.data));
+        $window.localStorage.chats = JSON.stringify($scope.chats);
         $scope.$apply();
-        $ionicScrollDelegate.scrollBottom(true);
     };
 
     $scope.sendMessage = function(){
         exampleSocket.send(JSON.stringify({"name": JSON.parse($window.localStorage.settings).username, "msg": $scope.message, "time": new Date()}));
         $scope.message = "";
     };
+
+    $scope.$watchCollection('chats', function(){
+        $ionicScrollDelegate.scrollBottom(true);
+    });
 })
 
 
